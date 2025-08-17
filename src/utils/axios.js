ca1b1ddfa0ec813cfe1253a6eb99dca57d82cc8b
Yash Singh <singhyash3012@gmail.com>
2025-08-17 18:51:15 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const LOCAL_URL = 'http://localhost:4040';
 
 const api = axios.create({
-  baseURL: LOCAL_URL,
+  baseURL: BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
